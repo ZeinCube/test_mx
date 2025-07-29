65435df738bde80025a3aa6ed7e6033a5cd435dd
Ivan 6'7 <kokorev.id@gmail.com>
2025-07-29 18:39:58 +1000

    Release Search

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -20,18 +20,4 @@ class BaseController
             'Content-Type' => 'text/html; charset=UTF-8'
         ]);
     }
-
-    protected function json(array $data, int $status = 200): Response
-    {
-        return new Response(
-            json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT),
-            $status,
-            ['Content-Type' => 'application/json; charset=UTF-8']
-        );
-    }
-
-    protected function redirect(string $url, int $status = 302): Response
-    {
-        return new Response('', $status, ['Location' => $url]);
-    }
-} 
+}
